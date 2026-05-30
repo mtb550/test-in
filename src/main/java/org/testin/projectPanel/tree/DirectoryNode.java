abe4f4930529b9514db701561967d47f702418cd
Muteb Almughyiri <mtb550@gmail.com>
2026-05-30 07:43:07 +0300

    new features

---
+++ src/main/java/org/testin/projectPanel/tree/DirectoryNode.java
@@ -0,0 +1,100 @@
+package org.testin.projectPanel.tree;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.projectView.PresentationData;
+import com.intellij.openapi.project.Project;
+import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.ui.treeStructure.SimpleNode;
+import org.jetbrains.annotations.NotNull;
+import org.testin.pojo.DirectoryMapper;
+import org.testin.pojo.DirectoryType;
+import org.testin.pojo.dto.dirs.*;
+import org.testin.projectPanel.ProjectPanel;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class DirectoryNode extends SimpleNode {
+
+    private final DirectoryDto directoryDto;
+    private final ProjectPanel projectPanel;
+
+    public DirectoryNode(Project project, DirectoryDto directoryDto, ProjectPanel projectPanel) {
+        super(project);
+        this.directoryDto = directoryDto;
+        this.projectPanel = projectPanel;
+    }
+
+    @Override
+    public SimpleNode @NotNull [] getChildren() {
+        if (directoryDto == null) return new SimpleNode[0];
+
+        List<SimpleNode> children = new ArrayList<>();
+
+        if (directoryDto instanceof TestProjectDirectoryDto projectDto) {
+            if (projectDto.getTestCasesDirectory() != null) {
+                children.add(new DirectoryNode(myProject, projectDto.getTestCasesDirectory(), projectPanel));
+            }
+            if (projectDto.getTestRunsDirectory() != null) {
+                children.add(new DirectoryNode(myProject, projectDto.getTestRunsDirectory(), projectPanel));
+            }
+            return children.toArray(new SimpleNode[0]);
+        }
+
+        Path currentPath = directoryDto.getPath();
+        if (currentPath != null && Files.exists(currentPath) && Files.isDirectory(currentPath)) {
+            try (Stream<Path> paths = Files.list(currentPath)) {
+                paths.map(this::mapPathToDirectory)
+                        .filter(Objects::nonNull)
+                        .forEachOrdered(childDto -> children.add(new DirectoryNode(myProject, childDto, projectPanel)));
+            } catch (Exception e) {
+                System.err.println("Failed to read directory for tree: " + e.getMessage());
+            }
+        }
+        return children.toArray(new SimpleNode[0]);
+    }
+
+    private DirectoryDto mapPathToDirectory(Path path) {
+        if (Files.exists(path.resolve(DirectoryType.TSP.getMarker())))
+            return DirectoryMapper.getInstance().testSetPackageNode(path, directoryDto);
+
+        if (Files.exists(path.resolve(DirectoryType.TS.getMarker())))
+            return DirectoryMapper.getInstance().testSetNode(path, directoryDto);
+
+        if (Files.exists(path.resolve(DirectoryType.TRP.getMarker())))
+            return DirectoryMapper.getInstance().testRunPackageNode(path, directoryDto);
+
+        if (Files.exists(path.resolve(DirectoryType.TR.getMarker())))
+            return DirectoryMapper.getInstance().testRunNode(path, directoryDto);
+
+        return null;
+    }
+
+    @Override
+    protected void update(@NotNull PresentationData presentation) {
+        super.update(presentation);
+        if (directoryDto == null) return;
+
+        DirectoryType type = Arrays.stream(DirectoryType.values())
+                .filter(t -> t.getClazz() == directoryDto.getClass())
+                .findFirst()
+                .orElse(null);
+
+        presentation.setIcon(type != null ? type.getIcon() : AllIcons.Nodes.Folder);
+
+        SimpleTextAttributes textAttr = (directoryDto instanceof TestCasesMainDirectoryDto || directoryDto instanceof TestRunsMainDirectoryDto)
+                ? SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES
+                : SimpleTextAttributes.REGULAR_ATTRIBUTES;
+
+        presentation.addText(directoryDto.getName(), textAttr);
+    }
+
+    public DirectoryDto getValue() {
+        return directoryDto;
+    }
+}
---
+++ src/main/java/org/testin/projectPanel/tree/ProjectTreeDnD.java
@@ -0,0 +1,167 @@
+package org.testin.projectPanel.tree;
+
+import com.intellij.ide.dnd.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.ui.tree.StructureTreeModel;
+import com.intellij.ui.treeStructure.SimpleTree;
+import com.intellij.util.ui.tree.TreeUtil;
+import org.jetbrains.annotations.NotNull;
+import org.testin.pojo.dto.dirs.DirectoryDto;
+import org.testin.util.TreeUtilImpl;
+
+import javax.swing.tree.TreePath;
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectTreeDnD {
+
+    public static final DataFlavor DTO_LIST_FLAVOR;
+
+    static {
+        try {
+            DTO_LIST_FLAVOR = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType + ";class=\"java.util.List\"");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to create custom DataFlavor", e);
+        }
+    }
+
+    public static void install(@NotNull SimpleTree tree, @NotNull Project project, @NotNull StructureTreeModel<?> structureModel) {
+        DnDManager manager = DnDManager.getInstance();
+        manager.registerSource(new TreeSource(tree), tree);
+        manager.registerTarget(new TreeTarget(tree, structureModel), tree);
+    }
+
+    private static DirectoryNode extractNode(TreePath path) {
+        if (path == null) return null;
+        Object userObject = TreeUtil.getUserObject(path.getLastPathComponent());
+        if (userObject instanceof DirectoryNode) {
+            return (DirectoryNode) userObject;
+        }
+        return null;
+    }
+
+    public record DtoTransferable(List<DirectoryDto> dtos) implements Transferable {
+
+        @Override
+        public DataFlavor[] getTransferDataFlavors() {
+            return new DataFlavor[]{DTO_LIST_FLAVOR, DataFlavor.javaFileListFlavor};
+        }
+
+        @Override
+        public boolean isDataFlavorSupported(DataFlavor flavor) {
+            return DTO_LIST_FLAVOR.equals(flavor) || DataFlavor.javaFileListFlavor.equals(flavor);
+        }
+
+        @NotNull
+        @Override
+        public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
+            if (DTO_LIST_FLAVOR.equals(flavor)) {
+                return dtos;
+            }
+
+            if (DataFlavor.javaFileListFlavor.equals(flavor)) {
+                List<File> files = new ArrayList<>();
+                for (DirectoryDto dto : dtos) {
+                    if (dto.getPath() != null) {
+                        files.add(dto.getPath().toFile());
+                    }
+                }
+                return files;
+            }
+
+            throw new UnsupportedFlavorException(flavor);
+        }
+    }
+
+    private record TreeSource(SimpleTree tree) implements DnDSource {
+
+        @Override
+        public boolean canStartDragging(DnDAction action, @NotNull Point dragOrigin) {
+            return tree.getSelectionPaths() != null && tree.getSelectionPaths().length > 0;
+        }
+
+        @Override
+        public DnDDragStartBean startDragging(DnDAction action, @NotNull Point dragOrigin) {
+            List<DirectoryDto> draggedData = new ArrayList<>();
+            TreePath[] paths = tree.getSelectionPaths();
+
+            if (paths != null) {
+                for (TreePath path : paths) {
+                    DirectoryNode node = extractNode(path);
+                    if (node != null && node.getValue() != null) {
+                        draggedData.add(node.getValue());
+                    }
+                }
+            }
+
+            return new DnDDragStartBean(new DtoTransferable(draggedData));
+        }
+    }
+
+    private record TreeTarget(SimpleTree tree, StructureTreeModel<?> structureModel) implements DnDTarget {
+
+        @Override
+        public boolean update(DnDEvent event) {
+            event.setDropPossible(false);
+
+            if (!(event.getAttachedObject() instanceof DtoTransferable)) return false;
+
+            TreePath dropPath = tree.getPathForLocation(event.getPoint().x, event.getPoint().y);
+            if (dropPath == null) return false;
+
+            DirectoryNode targetNode = extractNode(dropPath);
+            if (targetNode == null || targetNode.getValue() == null) return false;
+
+            event.setDropPossible(true);
+            return true;
+        }
+
+        @Override
+        public void drop(DnDEvent event) {
+            if (!(event.getAttachedObject() instanceof DtoTransferable(List<DirectoryDto> draggedDirs))) return;
+
+            TreePath dropPath = tree.getPathForLocation(event.getPoint().x, event.getPoint().y);
+            if (dropPath == null) return;
+
+            DirectoryNode targetNode = extractNode(dropPath);
+            if (targetNode == null) return;
+
+            DirectoryDto targetDir = targetNode.getValue();
+            boolean isMove = event.getAction() != null && event.getAction().getActionId() == DnDConstants.ACTION_MOVE;
+
+            for (DirectoryDto sourceDir : draggedDirs) {
+                if (isMove) {
+                    persistMove(sourceDir, targetDir);
+                } else {
+                    persistCopy(sourceDir, targetDir);
+                }
+            }
+
+            structureModel.invalidateAsync();
+        }
+
+        private void persistMove(DirectoryDto sourceDir, DirectoryDto targetDir) {
+            TreeUtilImpl.executeVfsAction(sourceDir.getPath(), targetDir.getPath(), "Move Failed", (sourceVf, targetVf) -> {
+                sourceVf.move(this, targetVf);
+                Path oldPath = sourceDir.getPath();
+                Path newPath = targetDir.getPath().resolve(sourceDir.getName());
+                sourceDir.setPath(newPath);
+                System.out.println("Moved successfully to: " + newPath);
+            });
+        }
+
+        private void persistCopy(DirectoryDto source, DirectoryDto target) {
+            TreeUtilImpl.executeVfsAction(source.getPath(), target.getPath(), "Copy Failed", (sourceVf, targetVf) -> {
+                sourceVf.copy(this, targetVf, sourceVf.getName());
+                System.out.println("Copied successfully to: " + target.getPath().resolve(source.getName()));
+            });
+        }
+    }
+}
